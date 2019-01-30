bac6208972d7eb7716c94cc9c970941bcf42b50e
okoala <dapixp@gmail.com>
2019-01-30 21:43:43 +0800

    feat: add home bottom nav bar

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class YQColor {
-  static Color primary1 = Color(0x25B864);
-  static Color primary2 = Color(0xF7F7F7);
-  static Color primary3 = Color(0xEDEEF0);
-  static Color primary4 = Color(0xFFFFFF);
-  static Color grey1 = Color(0x262626);
-  static Color grey2 = Color(0x404040);
-  static Color grey3 = Color(0x666666);
-  static Color grey4 = Color(0xA6A6A6);
-  static Color grey5 = Color(0xD9D9D9);
-  static Color blue1 = Color(0x3F6CA6);
+  static Color primary1 = Color(0xFF25B864);
+  static Color primary2 = Color(0xFFF7F7F7);
+  static Color primary3 = Color(0xFFEDEEF0);
+  static Color primary4 = Color(0xFFFFFFFF);
+  static Color grey1 = Color(0xFF262626);
+  static Color grey2 = Color(0xFF404040);
+  static Color grey3 = Color(0xFF666666);
+  static Color grey4 = Color(0xFFA6A6A6);
+  static Color grey5 = Color(0xFFD9D9D9);
+  static Color blue1 = Color(0xFF3F6CA6);
 }
---
+++ lib/pages/root_page/index.dart
@@ -0,0 +1,72 @@
+import "package:flutter/material.dart";
+import 'package:flutter/cupertino.dart';
+
+import 'package:yuque/config/theme.dart';
+
+import 'package:yuque/pages/yuque_page/index.dart';
+import 'package:yuque/pages/doc_page/index.dart';
+import 'package:yuque/pages/me_page/index.dart';
+
+class RootPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RootPageState();
+}
+
+class RootPageState extends State<RootPage> {
+  int tabIdx = 0;
+
+  List<Image> tabImages = [
+    Image.asset('assets/images/yuque-grey.png'),
+    Image.asset('assets/images/doc-grey.png'),
+    Image.asset('assets/images/me-grey.png'),
+  ];
+
+  List<Image> tabSelectedImages = [
+    Image.asset('assets/images/yuque-light.png'),
+    Image.asset('assets/images/doc-light.png'),
+    Image.asset('assets/images/me-light.png'),
+  ];
+
+  Image getTabIcon(int index) {
+    if (index == tabIdx) {
+      return tabSelectedImages[index];
+    } else {
+      return tabImages[index];
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        children: <Widget>[
+          YuquePage(),
+          DocPage(),
+          MePage()
+        ],
+        index: tabIdx,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: Colors.white,
+        activeColor: YQColor.primary1,
+        currentIndex: tabIdx,
+        onTap: (index) {
+          setState(() {
+            tabIdx = index;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: getTabIcon(0), title: Text('语雀')),
+          BottomNavigationBarItem(icon: getTabIcon(1), title: Text('文档')),
+          BottomNavigationBarItem(icon: getTabIcon(2), title: Text('我的')),
+        ],
+      ),
+    );
+  }
+}
+
