febc78a41fbca348e3f856e8f64ebfeb2080e185
okoala <dapixp@gmail.com>
2019-01-31 21:06:01 +0800

    feat: add login resources

--- lib/config/application.dart
+++ lib/config/application.dart
@@ -2,4 +2,7 @@ import 'package:fluro/fluro.dart';
 
 class Application {
   static Router router;
+  static Map<String, String> yuque = {
+    'settingTokenUrl': 'https://www.yuque.com/settings/tokens'
+  };
 }
