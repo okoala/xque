bd05cb37c550ecb567c69fde7bfbfacce2cd0b51
okoala <dapixp@gmail.com>
2019-01-31 15:49:09 +0800

    feat: add webview widget

---
+++ lib/pages/webview_page/index.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String url;
+  final String title;
+
+  WebViewPage({this.url, this.title});
+
+  WebViewPageState createState() => WebViewPageState();
+}
+
+class WebViewPageState extends State<WebViewPage> {
+  _openMoreSetting() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            onPressed: _openMoreSetting,
+            icon: Icon(Icons.more_horiz, size: 14,)
+          )
+        ],
+      ),
+      body: WebviewScaffold(
+        url: widget.url,
+        withZoom: false,
+        withJavascript: true,
+        withLocalStorage: true,
+        appCacheEnabled: true,
+      )
+    );
+  }
+}
