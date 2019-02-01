7d64ea3621ec221c23f66c86d673a87c0001c446
okoala <dapixp@gmail.com>
2019-02-01 13:05:20 +0800

    feat: login page

--- lib/pages/login_page/index.dart
+++ lib/pages/login_page/index.dart
@@ -8,13 +8,21 @@ class LoginPage extends StatefulWidget {
 class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          child: Image.asset('assets/images/yuque-light.png')
-        ),
-        Text('hello world')
-      ],
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 200, bottom: 10),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Image.asset('assets/images/yuque-logo.png', width: 80, height: 80, fit: BoxFit.cover,)
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text('欢迎回来', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),)
+            )
+          ],
+        )
+      )
     );
   }
 }
