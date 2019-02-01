362d140e200f8281edd4f2655969c5ef4dafdd5e
okoala <dapixp@gmail.com>
2019-02-01 17:03:51 +0800

    feat: webview scaffold

--- lib/pages/webview_page/index.dart
+++ lib/pages/webview_page/index.dart
@@ -18,38 +18,43 @@ class WebViewPageState extends State<WebViewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          backgroundColor: YQColor.grey7,
-          brightness: Brightness.light,
-          iconTheme: IconThemeData(color: YQColor.grey1),
-          textTheme: TextTheme(title: TextStyle(color: YQColor.grey1, fontSize: 18, fontWeight: FontWeight.w500)),
-          elevation: 0.0,
-          title: Text(widget.title),
-          leading: IconButton(
-            onPressed: () {
-              Application.router.pop(context);
-            },
-            icon: Icon(Icons.close, size: 30,)
-          ),
-          actions: <Widget>[
-            IconButton(
-              onPressed: _openMoreSetting,
-              icon: Icon(Icons.more_horiz, size: 30,)
-            )
-          ],
-        )
-      ),
-      body: WebviewScaffold(
+    return WebviewScaffold(
         url: widget.url,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: AppBar(
+            backgroundColor: YQColor.grey7,
+            brightness: Brightness.light,
+            iconTheme: IconThemeData(color: YQColor.grey1),
+            textTheme: TextTheme(title: TextStyle(color: YQColor.grey1, fontSize: 18, fontWeight: FontWeight.w500)),
+            elevation: 0.0,
+            title: Text(widget.title),
+            leading: IconButton(
+              onPressed: () {
+                Application.router.pop(context);
+              },
+              icon: Icon(Icons.close, size: 26,)
+            ),
+            actions: <Widget>[
+              IconButton(
+                onPressed: _openMoreSetting,
+                icon: Icon(Icons.more_horiz, size: 26,)
+              )
+            ],
+          )
+        ),
         userAgent: Application.yuque['useragent'],
-        withZoom: false,
+        withZoom: true,
         withJavascript: true,
         withLocalStorage: true,
         appCacheEnabled: true,
-      )
-    );
+        hidden: false,
+        initialChild: Container(
+          color: YQColor.grey1,
+          child: Center(
+            child: Text('加载中...'),
+          ),
+        ),
+      );
   }
 }
