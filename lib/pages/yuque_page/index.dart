f8ae038b002583d25b48b4092068f5ee7f114387
okoala <dapixp@gmail.com>
2019-02-04 22:40:46 +0800

    feat: move yuque_page scaffold to appscaffold

--- lib/pages/yuque_page/index.dart
+++ lib/pages/yuque_page/index.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:yuque/config/theme.dart';
+import 'package:yuque/components/app_scaffold.dart';
 import 'package:yuque/components/search_input.dart';
 import 'package:yuque/pages/yuque_page/group_cell.dart';
 
@@ -16,17 +17,7 @@ class YuquePage extends StatefulWidget {
 class YuquePageState extends State<YuquePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          backgroundColor: YQColor.grey7,
-          brightness: Brightness.light,
-          textTheme: TextTheme(title: TextStyle(color: YQColor.grey1, fontSize: 18, fontWeight: FontWeight.w500)),
-          elevation: 0.0,
-          title: Text('语雀'),
-        )
-      ),
+    return AppScaffold(
       body: Container(
         child: ListView(
           children: [
