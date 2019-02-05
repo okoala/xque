0d19da680408622b2d7844220e244caba0f20607
okoala <dapixp@gmail.com>
2019-02-05 14:25:29 +0800

    feat: use CupertinoNavigationBar

--- lib/components/app_scaffold.dart
+++ lib/components/app_scaffold.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:yuque/config/application.dart';
 import 'package:yuque/config/theme.dart';
+
+class AppNavBar extends StatelessWidget implements ObstructingPreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoNavigationBar(
+      backgroundColor: YQColor.grey7,
+      border: Border(
+        bottom: BorderSide(
+          color: YQColor.grey7,
+          width: 0.0, // One physical pixel.
+          style: BorderStyle.solid,
+        ),
+      ),
+      middle: Text(Application.yuque['name']),
+      leading: null,
+      // leading: CupertinoButton(
+      //   padding: EdgeInsets.zero,
+      //   onPressed: () {
+      //     Application.router.pop(context);
+      //   },
+      //   child: Icon(Icons.chevron_left, size: 30, color: YQColor.grey1,)
+      // ),
+    );
+  }
+
+  @override
+  bool get fullObstruction => true;
+
+  @override
+  Size get preferredSize {
+    return new Size.fromHeight(0.0);
+  }
+}
 
 class AppScaffold extends StatelessWidget {
   AppScaffold({ this.body });
@@ -9,18 +43,13 @@ class AppScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          backgroundColor: YQColor.grey7,
-          brightness: Brightness.light,
-          textTheme: TextTheme(title: TextStyle(color: YQColor.grey1, fontSize: 18, fontWeight: FontWeight.w500)),
-          elevation: 0.0,
-          title: Text(Application.yuque['name']),
-        )
+    return DefaultTextStyle(
+      style: CupertinoTheme.of(context).textTheme.textStyle,
+      child: CupertinoPageScaffold(
+        resizeToAvoidBottomInset: false,
+        navigationBar: AppNavBar(),
+        child: this.body,
       ),
-      body: this.body,
     );
   }
 }
