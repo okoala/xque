d8ddfc9aa5c0ab9ac5dcfd6e23b22d69cc111729
okoala <dapixp@gmail.com>
2019-02-08 21:53:55 +0800

    feat: improve CachedNetworkImage

--- lib/pages/yuque_page/index.dart
+++ lib/pages/yuque_page/index.dart
@@ -26,9 +26,19 @@ class YuquePageState extends State<YuquePage> {
             GroupCell(title: '我的消息', doctitle: '仙森邀请你来体验新APP',
             iconColor: YQColor.primary1,
             iconUrl: ClipOval(child:Image.asset('assets/images/notice.png'))),
-            GroupCell(title: 'Egg.js 团队', doctitle: 'https://eggjs.org', iconUrl: ClipOval(child: Image(
-              image: CachedNetworkImageProvider('https://lark-assets-prod.oss-cn-hangzhou.aliyuncs.com/2017/png/avatar/5a143e30-d777-4c45-9105-dd2abf33fd07.png?x-oss-process=image/resize,m_fill,w_112,h_112/format,png'),
-              fit: BoxFit.cover,))),
+            GroupCell(
+              title: 'Egg.js 团队',
+              doctitle: 'https://eggjs.org',
+              iconUrl: ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: 'https://lark-assets-prod.oss-cn-hangzhou.aliyuncs.com/2017/png/avatar/5a143e30-d777-4c45-9105-dd2abf33fd07.png?x-oss-process=image/resize,m_fill,w_112,h_112/format,png',
+                  errorWidget: new Icon(Icons.error),
+                  fadeOutDuration: new Duration(milliseconds: 300),
+                  fadeInDuration: new Duration(milliseconds: 300),
+                  fit: BoxFit.cover
+                ),
+              )
+            ),
             GroupCell(title: '订阅的消息', doctitle: '语雀: 深入浅出语雀编辑器',
             iconColor: YQColor.blue2, iconUrl: ClipOval(child: Image.asset('assets/images/notice.png', fit: BoxFit.cover,))),
           ]
