7fd8cc28112c12d7d77a6678174945f4ab985c12
okoala <dapixp@gmail.com>
2019-03-07 23:59:07 +0800

    feat: add anthor group

--- lib/pages/group_page/index.dart
+++ lib/pages/group_page/index.dart
@@ -17,9 +17,9 @@ class GroupPage extends StatefulWidget {
 }
 
 class GroupPageState extends State<GroupPage> {
-  Widget _buildBook() {
+  Widget _buildBook(String mainTitle, List<String>titles) {
     return Container(
-      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
       decoration: BoxDecoration(
         color: CupertinoColors.white,
         borderRadius: BorderRadius.circular(4.0),
@@ -44,7 +44,7 @@ class GroupPageState extends State<GroupPage> {
                   Expanded(
                     child: Container(
                       // padding: EdgeInsets.only(bottom: 10),
-                      child: Text('语雀使用帮助', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: YQColor.grey1),),
+                      child: Text(mainTitle, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: YQColor.grey1),),
                     ),
                   ),
                 ],
@@ -58,7 +58,7 @@ class GroupPageState extends State<GroupPage> {
             decoration:BoxDecoration(
               border: Border(bottom: BorderSide(width: 0.5, color: YQColor.grey7))
             ),
-            child: Text('深入浅出语雀编辑器', style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
+            child: Text(titles[0], style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
           ),
           Container(
             alignment: Alignment.topLeft,
@@ -67,7 +67,7 @@ class GroupPageState extends State<GroupPage> {
             decoration:BoxDecoration(
               border: Border(bottom: BorderSide(width: 0.5, color: YQColor.grey7))
             ),
-            child: Text('导出文档', style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
+            child: Text(titles[1], style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
           ),
           Container(
             alignment: Alignment.topLeft,
@@ -76,7 +76,7 @@ class GroupPageState extends State<GroupPage> {
             decoration:BoxDecoration(
               border: Border(bottom: BorderSide(width: 0.5, color: YQColor.grey7))
             ),
-            child: Text('Kitchen 更新日志', style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
+            child: Text(titles[2], style: TextStyle(fontSize: 16, color: YQColor.grey1, fontWeight: FontWeight.w400),)
           ),
           Container(
             alignment: Alignment.topLeft,
@@ -117,14 +117,15 @@ class GroupPageState extends State<GroupPage> {
                   alignment: Alignment.topCenter,
                   child: Text('语雀的天空', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: YQColor.grey1)),
                 ),
-                SizedBox(height: 20,),
+                SizedBox(height: 10,),
                 Container(
                   child: Text('知识创作与分享', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: YQColor.grey2),),
                 ),
-                SizedBox(height: 30,),
+                SizedBox(height: 40,),
                 Column(
                   children: <Widget>[
-                    _buildBook(),
+                    _buildBook('语雀使用文档', ['深入浅出语雀编辑器', '导出文档', 'Kitchen 更新日志']),
+                    _buildBook('语雀的天空', ['插入本地视频、查找替换、文本翻译，这几个功能让你的创作更easy！', '语雀月刊（2019年2月号）', '语雀更新剧透：除了独家功能“本地视频插入”，还有啥？']),
                   ],
                 )
               ],
